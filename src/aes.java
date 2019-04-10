e01b77c407498786f45cdcd23c0cc825d7e80404
Roy Ash <roykid123@gmail.com>
2019-04-10 21:09:04 +0300

    starting 6

---
+++ src/aes.java
@@ -0,0 +1,22 @@
+public class aes
+{
+	public static void main(String[] args)
+	{
+		if (args.length == 0)
+		{
+			System.out.println("No args!\nExiting...");
+			return;
+		}
+		switch (args[0].trim())
+		{
+			case "-e":
+				System.out.println(true);
+				break;
+			case "-d":
+
+				break;
+			case "-b":
+
+		}
+	}
+}
