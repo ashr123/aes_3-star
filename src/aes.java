82eb2328a0ca64b12c51eafd15feb65e33ab7547
Roy Ash <roykid123@gmail.com>
2019-04-11 00:28:45 +0300

    fdg

--- src/aes.java
+++ src/aes.java
@@ -1,13 +1,13 @@
-public class aes
+public final class aes
 {
-	public static void main(String[] args)
+	public static void main(final String[] args)
 	{
 		if (args.length == 0)
 		{
 			System.out.println("No args!\nExiting...");
 			return;
 		}
-		switch (args[0].trim())
+		switch (args[0])
 		{
 			case "-e":
 				System.out.println(true);
