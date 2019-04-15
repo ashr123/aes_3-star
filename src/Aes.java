2b6aed76b3db5562a4a9a14c17e52d46b824c067
Roy Ash <roykid123@gmail.com>
2019-04-15 04:06:56 +0300

    cs

--- src/Aes.java
+++ src/Aes.java
@@ -48,33 +48,61 @@ public final class Aes
 		}
 	}
 
+	/**
+	 * Reads a file's data by bytes and saving it's byte in a matrices of 4x4 (a block of 16 bytes).
+	 *
+	 * @param file the file to be read.
+	 * @return a 3 dimensional matrix of data like so: numberOfBlocks x 4 x 4.
+	 * @throws IOException if any error related to reading or parsing the file happens.
+	 */
 	private static byte[][][] readFile(final File file) throws IOException
 	{
 		try (FileInputStream fileInputStream=new FileInputStream(file))
 		{
-			final int blockSize=(int) file.length()/16; // for separating to blocks of 128 bits (128 bits=16 bytes)
-			final byte[][][] keys=new byte[blockSize][4][4];
-			for (int i=0; i<blockSize; i++)
-				for (int j=0; j<4; j++)
-					if (fileInputStream.read(keys[i][j])==-1)
-						throw new IOException("Not enough bits for 3 keys!!!");
-			return keys;
+			final int numOfBlocks=(int) file.length()/16; // for separating to blocks of 128 bits (128 bits=16 bytes)
+			final byte[][][] data=new byte[numOfBlocks][4][4];
+			for (int i=0; i<numOfBlocks; i++)
+				for (int j=0; j<data.length; j++)
+					if (fileInputStream.read(data[i][j])==-1)
+						throw new IOException(
+								"Not enough bytes for hole block of 16 bytes in file "+file.getName()+"! (num of bytes: "+file.length()+')');
+			return data;
 		}
 	}
 
+	/**
+	 * Incorporates a key into the message.
+	 *
+	 * @param msg The original message.
+	 * @param key The key to incorporate.
+	 */
 	private static void addRoundKey(final byte[][] msg, final byte[][] key)
 	{
-		for (int i=0; i<4; i++)
-			for (int j=0; j<4; j++)
+		for (int i=0; i<msg.length; i++)
+			for (int j=0; j<msg[i].length; j++)
 				msg[i][j]=(byte) (msg[i][j] ^ key[i][j]);
 	}
 
+	/**
+	 * Shifts every line right or left by it's line number.
+	 *
+	 * @param msg          The original message.
+	 * @param isToTheRight If {@code true}, shifts to the right, else, shifts to the left.
+	 */
 	private static void shiftRows(final byte[][] msg, final boolean isToTheRight)
 	{
-		for (int i=0; i<4; i++)
+		for (int i=0; i<msg.length; i++)
 			rotate(msg[i], isToTheRight ? i : -i);
 	}
 
+	/**
+	 * Encrypts a file with AES-3-star algorithm.
+	 *
+	 * @param keysFile   A key file, should contain 3 keys 16 bytes long.
+	 * @param inputFile  An input file, an original message, it's size (in bytes) is a multiplication of 16.
+	 * @param outputFile The result of the encryption, a cipher file.
+	 * @throws IOException If there is an error concerning any of the files.
+	 */
 	private static void encrypt(final File keysFile, final File inputFile, final File outputFile) throws IOException
 	{
 		final byte[][][]
@@ -83,10 +111,18 @@ public final class Aes
 		//		if (!outputFile.delete())
 		//			throw new IOException("can't delete "+outputFile.getName());
 		if (!outputFile.createNewFile())
-			throw new IOException("can't create new file with name: "+outputFile.getName());
+			throw new IOException("Can't create new file with name: "+outputFile.getName());
 
 	}
 
+	/**
+	 * Decrypts a file with AES-3-star algorithm.
+	 *
+	 * @param keysFile   A key file, should contain 3 keys 16 bytes long.
+	 * @param inputFile  An input file, an original message, it's size (in bytes) is a multiplication of 16.
+	 * @param outputFile The result of the encryption, a cipher file.
+	 * @throws IOException If there is an error concerning any of the files.
+	 */
 	private static void decrypt(final File keysFile, final File inputFile, final File outputFile) throws IOException
 	{
 		final byte[][][]
@@ -95,7 +131,7 @@ public final class Aes
 		//		if (!outputFile.delete())
 		//			throw new IOException("can't delete "+outputFile.getName());
 		if (!outputFile.createNewFile())
-			throw new IOException("can't create new file with name: "+outputFile.getName());
+			throw new IOException("Can't create new file with name: "+outputFile.getName());
 
 	}
 
@@ -105,7 +141,8 @@ public final class Aes
 		//			throw new IllegalArgumentException("Not enough args!");
 
 		final List<String> argsList=Arrays.asList(args);
-		final File file1, file2, file3=new File(args[argsList.indexOf("-o")+1]);;
+		final File file1, file2, file3=new File(args[argsList.indexOf("-o")+1]);
+		;
 		if (args[0].equals("-e") || args[0].equals("-d"))
 		{
 			file1=new File(args[argsList.indexOf("-k")+1]);
